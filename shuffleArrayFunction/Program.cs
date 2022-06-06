7bdf37ca02efe501bbdbb827c476deb700f1ee5a
Михаил Денисов <migelius.invesy@gmail.com>
2022-06-06 18:30:04 +0300

    Добавьте файлы проекта.

---
+++ shuffleArrayFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayNumbers = { 1, 2, 3, 4, 5, 6 };
+        ArrayScreen(arrayNumbers);
+        arrayNumbers = ArrayShuffle(arrayNumbers);
+        ArrayScreen(arrayNumbers);
+
+    }
+
+    static int[] ArrayShuffle(int[] array)
+    {
+        Random random = new Random();
+
+        for (int i = array.Length - 1; i >= 1; i--)
+        {
+            int j = random.Next(i + 1);
+            int temporary = array[j];
+            array[j] = array[i];
+            array[i] = temporary;
+        }
+        return array;
+    }
+
+    static void ArrayScreen(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
